c7ce3f384fc14ba7933ab583902f9d668bfe7062
dcmallett <dcmallett28@gmail.com>
2024-07-28 01:11:54 +0100

    Adding ternary to determin btn text

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,7 +6,8 @@ export default function Player({ name, symbol}) {
 
   //add function that is triggered when button is clicked
   const handleEditClick = () => {
-    setIsEditing(true)
+    setIsEditing(!isEditing)
+
   }
   //chage is setIsEditing to true
   //show span player-name when editing is false
@@ -16,10 +17,10 @@ export default function Player({ name, symbol}) {
       <li>
       <span className="player">
         {!isEditing && <span className="player-name">{name}</span> }
-        {isEditing && <input type="text" required /> }
+        {isEditing && <input type="text" required value={name} /> }
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   )
 }
