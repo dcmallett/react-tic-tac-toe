7810939e53ccb8cf35dc6c23e473980ab22515da
dcmallett <dcmallett28@gmail.com>
2024-07-29 16:10:24 +0100

    Amended the useState to a function

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol}) {
+export default function Player({ initialName, symbol}) {
 
+  const  [ playerName, setPlayerName ] = useState(initialName);
   const [ isEditing, setIsEditing ] = useState(false); 
 
-  //add function that is triggered when button is clicked
   const handleEditClick = () => {
-    setIsEditing(!isEditing)
-
+    //To update the state correctly. You need to pass in a function
+    //into the state that you want to update
+    //using the function form. You garentee you are always updating the 
+    //previous state value
+    setIsEditing((editing) => !isEditing)
   }
-  //chage is setIsEditing to true
-  //show span player-name when editing is false
-  //show input when isEditing is true
+
+  //function for change when you change the player name
+  // becuase on change listens for every key stroke. You need to listen to the event
+  //So you need to pass an event param / prop into the change function
+  function handleChange(event) {
+    //Here the setPlayerName state is listening for the value on the target event.
+    //TWO WAY BINDING
+    setPlayerName(event.target.value)
+  }
+
 
   return (
       <li>
       <span className="player">
-        {!isEditing && <span className="player-name">{name}</span> }
-        {isEditing && <input type="text" required value={name} /> }
+        {!isEditing && <span className="player-name">{playerName}</span> }
+        
+        {isEditing && <input type="text" required value={playerName} onChange={handleChange} /> }
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
