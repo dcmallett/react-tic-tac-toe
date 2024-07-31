3d67820a4da9e0a76d838f611944cb9be72b4706
dcmallett <dcmallett28@gmail.com>
2024-07-31 14:27:25 +0100

    Adding game board functionallity

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+const intialGameBoard = [
+  //multi-dimensional array
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+
+  const [gameBoard, setGameBoard] = useState(intialGameBoard)
+
+  function handleSelectSquare(rowIndex, colsIndex) {
+    setGameBoard((prevGameBoard) => {
+      //if we are updating arrays or objs we need to do it in an immuatable way.
+      //so we need to take a copy of the state in this case prevGameBoard. and we then pass that
+      //into a const / variable to be used elsewhere. if we are using nested arrays they need to be copied as well.
+      // by using the map method
+      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+      updatedBoard[rowIndex][colsIndex] = activePlayerSymbol
+      return updatedBoard;
+    })
+
+    onSelectSquare();
+  }
+
+
+  return (
+    <ol id="game-board">
+      {/* 
+        its not good use case to use the index in case any data gets swapped around as the 
+        index is not tied to the data but to the postion of the data. but this use case is fine
+        as we wont be swapping data around normally it would be an ID thats used
+      */}
+      {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        <ol>
+          {row.map((playerSymbol, colsIndex) => <li key={colsIndex}>
+            <button 
+              onClick={() => handleSelectSquare(rowIndex, colsIndex)}>
+                {playerSymbol}
+            </button>
+          </li>)}
+        </ol>
+      </li>)}
+    </ol>
+  )
+}
